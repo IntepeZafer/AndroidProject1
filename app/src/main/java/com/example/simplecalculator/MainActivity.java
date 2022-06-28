ebd97136b22fb4484b282451c56136e6ac4749ab
IntepeZafer <intepezafer@gmail.com>
2022-06-28 16:56:12 +0300

    This repository contains my first android project that I developed. The purpose of the project is to get 2 numbers from the user and perform 4 actions.

---
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editTextTextPersonName3;
+    EditText editTextTextPersonName4;
+    TextView textView5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextTextPersonName3 = findViewById(R.id.editTextTextPersonName3);
+        editTextTextPersonName4 = findViewById(R.id.editTextTextPersonName4);
+        textView5 = findViewById(R.id.textView5);
+    }
+    // Define onClick actions
+    public void sum(View view){
+        int myNumber1 = Integer.parseInt(editTextTextPersonName3.getText().toString());
+        int myNumber2 = Integer.parseInt(editTextTextPersonName4.getText().toString());
+        int result = myNumber1 + myNumber2;
+        textView5.setText("Result: " + result);
+    }
+    public void deduct(View view){
+        int myNumber1 = Integer.parseInt(editTextTextPersonName3.getText().toString());
+        int myNumber2 = Integer.parseInt(editTextTextPersonName4.getText().toString());
+        int result = myNumber1 - myNumber2;
+        textView5.setText("Result: " + result);
+    }
+    public void multiply(View view){
+        int myNumber1 = Integer.parseInt(editTextTextPersonName3.getText().toString());
+        int myNumber2 = Integer.parseInt(editTextTextPersonName4.getText().toString());
+        int result = myNumber1 * myNumber2;
+        textView5.setText("Result: " +       result);
+    }
+    public void divide(View view){
+        double myNumber1 = Double.parseDouble(editTextTextPersonName3.getText().toString());
+        double myNumber2 = Double.parseDouble(editTextTextPersonName4.getText().toString());
+        double result = myNumber1 / myNumber2;
+        textView5.setText("Result: " + result);
+    }
+}
