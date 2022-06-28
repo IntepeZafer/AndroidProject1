f7a0586a15b4cb06723d351dda6f8f7c50becd38
IntepeZafer <intepezafer@gmail.com>
2022-06-28 18:07:13 +0300

    This repository contains my first android project that I developed. The purpose of the project is to get 2 numbers from the user and perform 4 actions.

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -23,27 +23,43 @@ public class MainActivity extends AppCompatActivity {
     }
     // Define onClick actions
     public void sum(View view){
-        int myNumber1 = Integer.parseInt(editTextTextPersonName3.getText().toString());
-        int myNumber2 = Integer.parseInt(editTextTextPersonName4.getText().toString());
-        int result = myNumber1 + myNumber2;
-        textView5.setText("Result: " + result);
+        if(editTextTextPersonName3.getText().toString().matches("") || editTextTextPersonName4.getText().toString().matches("")){
+            textView5.setText("Enter Number!");
+        }else{
+            int myNumber1 = Integer.parseInt(editTextTextPersonName3.getText().toString());
+            int myNumber2 = Integer.parseInt(editTextTextPersonName4.getText().toString());
+            int result = myNumber1 + myNumber2;
+            textView5.setText("Result: " + result);
+        }
     }
     public void deduct(View view){
-        int myNumber1 = Integer.parseInt(editTextTextPersonName3.getText().toString());
-        int myNumber2 = Integer.parseInt(editTextTextPersonName4.getText().toString());
-        int result = myNumber1 - myNumber2;
-        textView5.setText("Result: " + result);
+        if(editTextTextPersonName3.getText().toString().matches("") || editTextTextPersonName4.getText().toString().matches("")){
+            textView5.setText("Enter Number!");
+        }else{
+            int myNumber1 = Integer.parseInt(editTextTextPersonName3.getText().toString());
+            int myNumber2 = Integer.parseInt(editTextTextPersonName4.getText().toString());
+            int result = myNumber1 - myNumber2;
+            textView5.setText("Result: " + result);
+        }
     }
     public void multiply(View view){
-        int myNumber1 = Integer.parseInt(editTextTextPersonName3.getText().toString());
-        int myNumber2 = Integer.parseInt(editTextTextPersonName4.getText().toString());
-        int result = myNumber1 * myNumber2;
-        textView5.setText("Result: " +       result);
+        if(editTextTextPersonName3.getText().toString().matches("") || editTextTextPersonName4.getText().toString().matches("")){
+            textView5.setText("Enter Number!");
+        }else{
+            int myNumber1 = Integer.parseInt(editTextTextPersonName3.getText().toString());
+            int myNumber2 = Integer.parseInt(editTextTextPersonName4.getText().toString());
+            int result = myNumber1 * myNumber2;
+            textView5.setText("Result: " +       result);
+        }
     }
     public void divide(View view){
-        double myNumber1 = Double.parseDouble(editTextTextPersonName3.getText().toString());
-        double myNumber2 = Double.parseDouble(editTextTextPersonName4.getText().toString());
-        double result = myNumber1 / myNumber2;
-        textView5.setText("Result: " + result);
+        if(editTextTextPersonName3.getText().toString().matches("") || editTextTextPersonName4.getText().toString().matches("")){
+            textView5.setText("Enter Number!");
+        }else{
+            double myNumber1 = Double.parseDouble(editTextTextPersonName3.getText().toString());
+            double myNumber2 = Double.parseDouble(editTextTextPersonName4.getText().toString());
+            double result = myNumber1 / myNumber2;
+            textView5.setText("Result: " + result);
+        }
     }
 }
